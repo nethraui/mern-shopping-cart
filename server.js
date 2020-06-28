c3c865057e20173b653276f00defddaa80d5a529
nethraui <nethra.uim@gmail.com>
2020-06-28 16:54:07 -0400

    fix

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
+const path = require('path')
 
 const users = require("./routes/api/users");
 
@@ -37,7 +38,9 @@ require("./config/passport")(passport);
 app.use("/api/users", users);
 // Routes
 app.use('/api/products', require('./routes/products'));
-
+app.use(express.static(path.join(__dirname, 'client/build')))
+app.get('*', (req, res) => {
+	    res.sendFile(path.join(__dirname, 'client/build'))
+})
 const port = process.env.PORT || 8000;
-
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
